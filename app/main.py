18185df40fe3342bdb4c1d28492284a1acd5964b
vphantomk666 <vidyadavi644@gmail.com>
2026-03-29 20:55:24 +0530

    Add favicon and static file mounting

--- app/main.py
+++ app/main.py
@@ -1,15 +1,20 @@
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from app.routes.routes import router
 
 app = FastAPI()
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "../static")), name="static")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://127.0.0.1:5500/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
