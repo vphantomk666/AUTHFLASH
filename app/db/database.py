5591e5f4d1162c185453cc8a96ec9ec152b6ca80
vphantomk666 <vidyadavi644@gmail.com>
2026-04-19 20:46:21 +0530

    Switch to PostgreSQL for Render

--- app/db/database.py
+++ app/db/database.py
@@ -2,9 +2,6 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 from dotenv import load_dotenv
-import pymysql
-
-pymysql.install_as_MySQLdb()
 
 load_dotenv()
 
@@ -13,10 +10,12 @@ DATABASE_URL = os.getenv("DataBase_URL")
 if DATABASE_URL is None:
     raise ValueError("DataBase_URL is not set")
 
-db_name = "Authenticaton"
+db_name = "authenticaton"
+
+DATABASE_URL_without_db = DATABASE_URL.rsplit("/", 1)[0]
 
 engine = create_engine(
-    DATABASE_URL.replace(f"/{db_name}", "/mysql"),
+    DATABASE_URL_without_db,
     pool_pre_ping=True,
     pool_recycle=300
 )
